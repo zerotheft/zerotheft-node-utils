830db3bb8e57e9f049bd5249f5bb272da3598cbd
racetrack0616 <racetrack0616@gmail.com>
2021-07-23 09:45:45 +0545

    update feedbacks methods

---
+++ contracts/feedbacks.js
@@ -0,0 +1,27 @@
+const { getFeedbackContract } = require('../utils/contract')
+
+
+/**
+ * Get the version of feedback contract version
+ * @param {object} getFeedbackContract Instance of feedback contract
+ * @returns Object with feedback contract version information
+ */
+const getFeedbackContractVersion = async (feedbackContract = null) => {
+  if (!feedbackContract) {
+    feedbackContract = await getFeedbackContract()
+  }
+  try {
+    const version = await feedbackContract.callSmartContractGetFunc('getContractVersion')
+    return {
+      success: true,
+      version,
+      number: version.split('v')[1]
+    }
+  } catch (e) {
+    return { success: false, error: e.message }
+  }
+}
+
+module.exports = {
+  getFeedbackContractVersion
+}
