6bc0a1164331afeb1327602aa6a39768c6cc12a8
racetrack0616 <racetrack0616@gmail.com>
2021-09-27 10:58:14 +0545

    feedback version retured properly

--- contracts/feedbacks.js
+++ contracts/feedbacks.js
@@ -2,7 +2,7 @@ const { getFeedbackContract } = require('../utils/contract')
 
 /**
  * Get the version of feedback contract version
- * @param {object} getFeedbackContract Instance of feedback contract
+ * @param {object} feedbackContract Instance of feedback contract
  * @returns Object with feedback contract version information
  */
 const getFeedbackContractVersion = async (feedbackContract = null) => {
@@ -10,15 +10,16 @@ const getFeedbackContractVersion = async (feedbackContract = null) => {
     feedbackContract = await getFeedbackContract()
   }
   try {
-    const version = await feedbackContract.callSmartContractGetFunc('getContractVersion')
+    const versionNumber = await feedbackContract.callSmartContractGetFunc('getContractVersion')
     return {
       success: true,
-      version,
-      number: version.split('v')[1],
+      version: `v${versionNumber}`,
+      number: versionNumber,
     }
   } catch (e) {
     return { success: false, error: e.message }
   }
+
 }
 
 module.exports = {
